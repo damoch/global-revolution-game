c7f70c9710c8e1c0b85277cdbbee121551d2c09e
Damoch <damochramowicz260@gmail.com>
2020-02-23 12:01:14 +0100

    countries and cities

---
+++ Global Revolution/Assets/Scripts/World/City.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.World
+{
+    public class City : MonoBehaviour
+    {
+        [SerializeField]
+        private string _name;
+
+        [SerializeField]
+        private bool _isCapitalCity;
+
+        private Country _country;
+
+        public bool IsCapitalCity { get => _isCapitalCity; }
+
+        public void InjectCountry(Country country)
+        {
+            _country = country;
+        }
+    }
+}
---
+++ Global Revolution/Assets/Scripts/World/Country.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.World
+{
+    public class Country : MonoBehaviour
+    {
+        [SerializeField]
+        private string _name;
+
+        [SerializeField]
+        private SpriteRenderer _teritoryRenderer;
+
+        [SerializeField]
+        private City[] _cities;
+
+        private City _capital;
+        private void Start()
+        {
+            _teritoryRenderer = GetComponent<SpriteRenderer>();
+
+            InjectToCities();
+        }
+
+        private void InjectToCities()
+        {
+            
+            foreach (var city in _cities)
+            {
+                if(city.IsCapitalCity)
+                {
+                    _capital = city;
+                }
+                city.InjectCountry(this);
+            }
+        }
+    }
+}
